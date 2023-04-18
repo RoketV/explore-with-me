00f343e5a40cfd8c1aadf5e8d41458b76c6ead5c
Vlados <vladislavmoroz96@yandex.ru>
2023-04-18 10:36:23 +0300

    fix: checkstyle

--- statistics/stat-dtos/src/main/java/com/explore/statdtos/dtos/EndPointHitDto.java
+++ statistics/stat-dtos/src/main/java/com/explore/statdtos/dtos/EndPointHitDto.java
@@ -7,41 +7,41 @@ import java.time.LocalDateTime;
 
 
 public class EndPointHitDto {
-        private final Long id;
-        @NotBlank(message = "app is empty or null")
-        private final String app;
-        @NotBlank(message = "uri is empty or null")
-        private final String uri;
-        @NotBlank(message = "ip is empty or null")
-        private final String ip;
-        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
-        private final LocalDateTime timestamp;
+    private final Long id;
+    @NotBlank(message = "app is empty or null")
+    private final String app;
+    @NotBlank(message = "uri is empty or null")
+    private final String uri;
+    @NotBlank(message = "ip is empty or null")
+    private final String ip;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private final LocalDateTime timestamp;
 
-        public EndPointHitDto(Long id, String app, String uri, String ip, LocalDateTime timestamp) {
-                this.id = id;
-                this.app = app;
-                this.uri = uri;
-                this.ip = ip;
-                this.timestamp = timestamp;
-        }
+    public EndPointHitDto(Long id, String app, String uri, String ip, LocalDateTime timestamp) {
+        this.id = id;
+        this.app = app;
+        this.uri = uri;
+        this.ip = ip;
+        this.timestamp = timestamp;
+    }
 
-        public Long id() {
-                return id;
-        }
+    public Long id() {
+        return id;
+    }
 
-        public String app() {
-                return app;
-        }
+    public String app() {
+        return app;
+    }
 
-        public String uri() {
-                return uri;
-        }
+    public String uri() {
+        return uri;
+    }
 
-        public String ip() {
-                return ip;
-        }
+    public String ip() {
+        return ip;
+    }
 
-        public LocalDateTime timestamp() {
-                return timestamp;
-        }
+    public LocalDateTime timestamp() {
+        return timestamp;
+    }
 }
--- statistics/stat-dtos/src/main/java/com/explore/statdtos/dtos/ViewStatsDto.java
+++ statistics/stat-dtos/src/main/java/com/explore/statdtos/dtos/ViewStatsDto.java
@@ -2,13 +2,13 @@ package com.explore.statdtos.dtos;
 
 import javax.validation.constraints.NotNull;
 
-public class ViewStatsDto{
-        @NotNull
-        private final String app;
-        @NotNull
-        private final String uri;
-        @NotNull
-        private final Long hits;
+public class ViewStatsDto {
+    @NotNull
+    private final String app;
+    @NotNull
+    private final String uri;
+    @NotNull
+    private final Long hits;
 
     public ViewStatsDto(String app, String uri, Long hits) {
         this.app = app;
